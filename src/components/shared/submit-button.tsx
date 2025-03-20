ddbbf167b69e80dcedda0492df2df4ac705de65c
Evandro Calado <evandrocalado07@gmail.com>
2025-03-20 10:55:46 -0300

    refactor: enhance SubmitButton component to accept custom className and improve loading state styling

--- src/components/shared/submit-button.tsx
+++ src/components/shared/submit-button.tsx
@@ -3,25 +3,34 @@
 import { Loader2 } from 'lucide-react';
 import { useFormStatus } from 'react-dom';
 
+import { cn } from '@/lib/utils';
 import { Button } from '../ui/button';
 
 type SubmitButtonProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export const SubmitButton = ({ children }: SubmitButtonProps) => {
+export const SubmitButton = ({ children, className }: SubmitButtonProps) => {
   const { pending } = useFormStatus();
 
   return (
     <>
       {pending ? (
-        <Button className='w-full' disabled={pending}>
+        <Button
+          className={cn('flex w-full items-center gap-2', className)}
+          disabled={pending}
+        >
           <Loader2 className='size-4 animate-spin' />
-          <span>Carregando</span>
+          Carregando
         </Button>
       ) : (
-        <Button type='submit' className='w-full' disabled={pending}>
-          <span>{children}</span>
+        <Button
+          type='submit'
+          className={cn('flex w-full items-center gap-2', className)}
+          disabled={pending}
+        >
+          {children}
         </Button>
       )}
     </>
